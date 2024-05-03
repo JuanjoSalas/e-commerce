a73fd5128b7937d5d50028a4c6d80f6b579a9b0b
juan salas <juanjo.salas.dev@gmail.com>
2024-05-03 11:50:34 +0200

    models

--- models/game.js
+++ models/game.js
@@ -10,7 +10,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Game.belongsToMany(models.Genre,{
+        through:models.GenreGame
+      })
+      Game.belongsToMany(models.Order,{
+        through:models.OrderGame
+      })
     }
   }
   Game.init({
--- models/order.js
+++ models/order.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Order.belongsTo(models.User);
+      Order.belongsToMany(models.Game,{
+        through:models.OrderGame
+      })
     }
   }
   Order.init({
